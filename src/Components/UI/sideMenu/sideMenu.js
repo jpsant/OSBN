2a69126f6fd3a32b21e6c86cebbb0474cfa5fe26
Joao Paulo <j.paaulinho@gmail.com>
2019-05-23 20:36:48 -0300

    tolltips adicionadas com sucesso

--- src/Components/UI/sideMenu/sideMenu.js
+++ src/Components/UI/sideMenu/sideMenu.js
@@ -1,23 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './css/sideMenu.module.css';
 
-const sideMenu = (props) => {
-    return (
-        <div className={classes.menu}
-            style={{
-                transform: props.show ? 'translateX(0)' : 'translateX(100vh)',
-                opacity: props.show ? '1' : '0'
-            }}
-        >
-            <h2 className={classes.title}>&</h2>
-            <button className={props.section == 'history' ? classes.historyButton + ' ' + classes.active : classes.historyButton}><img alt="" className={classes.svg} src={require('../../../assets/svgs/book.svg')}></img></button>
-            <button className={props.section == 'gallery' ? classes.historyButton + ' ' + classes.active : classes.historyButton}><img alt="" className={classes.svg} src={require('../../../assets/svgs/portrait.svg')}></img></button>
-            <button className={props.section == 'schedule' ? classes.historyButton + ' ' + classes.active : classes.historyButton}><img alt="" className={classes.svg} src={require('../../../assets/svgs/calendar.svg')}></img></button>
-            <button className={props.section == 'news' ? classes.historyButton + ' ' + classes.active : classes.historyButton}><img alt="" className={classes.svg} src={require('../../../assets/svgs/news.svg')}></img></button>
-            <button className={props.section == 'technical' ? classes.historyButton + ' ' + classes.active : classes.historyButton}><img alt="" className={classes.svg} src={require('../../../assets/svgs/accordion.svg')}></img></button>
-            <button className={props.section == 'contact' ? classes.historyButton + ' ' + classes.active : classes.historyButton}><img alt="" className={classes.svg} src={require('../../../assets/svgs/facebook.svg')}></img></button>
-        </div>
-    );
+class sideMenu extends Component {
+
+    state = {
+        show: true
+    }
+
+    onCloseHandler = () => {
+        this.setState({ show: false })
+    }
+
+    render() {
+
+        return (
+            <div className={classes.menu}
+                style={{
+                    transform: this.props.show && this.state.show ? 'translateX(0)' : 'translateX(100vh)',
+                    opacity: this.props.show ? '1' : '0'
+                }}
+            >
+                <div className={classes.closeButton}>
+                    <span className={classes.tooltipText}>Fechar Menu</span>
+                    <h2 onClick={this.onCloseHandler} className={classes.title}>&</h2>
+                </div>
+                <div className={classes.historyButton}>
+                    <span className={classes.historyTooltip}>Histórico</span>
+                    <button className={this.props.section == 'history' ? classes.Button + ' ' + classes.active : classes.Button}><img alt="" className={classes.svg} src={require('../../../assets/svgs/book.svg')}></img></button>
+                </div>
+                <div className={classes.galleryButton}>
+                    <span className={classes.galleryTooltip}>Galeria</span>
+                    <button className={this.props.section == 'gallery' ? classes.Button + ' ' + classes.active : classes.Button}><img alt="" className={classes.svg} src={require('../../../assets/svgs/portrait.svg')}></img></button>
+                </div>
+                <div className={classes.scheduleButton}>
+                    <span className={classes.scheduleTooltip}>Agenda</span>
+                    <button className={this.props.section == 'schedule' ? classes.Button + ' ' + classes.active : classes.Button}><img alt="" className={classes.svg} src={require('../../../assets/svgs/calendar.svg')}></img></button>
+                </div>
+                <div className={classes.newsButton}>
+                    <span className={classes.newsTooltip}>Notícias</span>
+                    <button className={this.props.section == 'news' ? classes.Button + ' ' + classes.active : classes.Button}><img alt="" className={classes.svg} src={require('../../../assets/svgs/news.svg')}></img></button>
+                </div>
+                <div className={classes.techButton}>
+                    <span className={classes.techTooltip}>Tecnica</span>
+                    <button className={this.props.section == 'technical' ? classes.Button + ' ' + classes.active : classes.Button}><img alt="" className={classes.svg} src={require('../../../assets/svgs/accordion.svg')}></img></button>
+                </div>
+                <div className={classes.socialButton}>
+                    <span className={classes.socialTooltip}>Social</span>
+                    <button className={this.props.section == 'contact' ? classes.Button + ' ' + classes.active : classes.Button}><img alt="" className={classes.svg} src={require('../../../assets/svgs/facebook.svg')}></img></button>
+                </div>
+
+            </div>
+        );
+    }
 }
 
 export default sideMenu;
