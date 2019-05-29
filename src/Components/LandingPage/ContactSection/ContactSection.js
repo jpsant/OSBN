31254a90d84889a12d8a5b714e3f62d722c22dc5
Joao Paulo <j.paaulinho@gmail.com>
2019-05-28 23:12:34 -0300

    footer inicializado

--- src/Components/LandingPage/ContactSection/ContactSection.js
+++ src/Components/LandingPage/ContactSection/ContactSection.js
@@ -7,31 +7,31 @@ import TransitionDiv from '../../UI/transitionDiv/history/historyDiv';
 class ContactSection extends Component {
     render() {
         return (
-            <div style={{backgroundColor: '#AC7C44'}}>
-                <TransitionDiv title={this.props.language === 'portuguese' ? '& Contato' : 
-                this.props.language === 'english' ? '& Contact' : 
-                this.props.language === 'french' ? '& Nous contacter' : ''} />
+            <div style={{ backgroundColor: '#AC7C44' }}>
+                <TransitionDiv title={this.props.language === 'portuguese' ? '& Contato' :
+                    this.props.language === 'english' ? '& Contact' :
+                        this.props.language === 'french' ? '& Nous contacter' : ''} />
                 <div className={classes.contactContainer}>
                     <div className={classes.titleContainer}>
                         <h1 className={classes.title}>Orquestra Sanfônica Balaio Nordeste! <span>&</span></h1>
                     </div>
                     <div className={classes.social}>
-                        <h1>{this.props.language === 'portuguese' ? 'Nos siga nas Redes Sociais!' : 
-                            this.props.language === 'english' ? 'Follow us on Social Media!' : 
-                            this.props.language === 'french' ? 'Suivez nous sur les réseaux sociaux!' : ''}</h1>
+                        <h1>{this.props.language === 'portuguese' ? 'Nos siga nas Redes Sociais!' :
+                            this.props.language === 'english' ? 'Follow us on Social Media!' :
+                                this.props.language === 'french' ? 'Suivez nous sur les réseaux sociaux!' : ''}</h1>
                         <div className={classes.socialMedia}>
-                            <h2>twitter</h2>
-                            <h2>instagram</h2>
-                            <h2>facebook</h2>
+                            <a href="https://www.youtube.com/channel/UCM6Z3B0OVdpLU8b7Qxe0hEA"><img className={classes.svg} src={require('../../../assets/svgs/youtubeFooter.svg')} alt=""></img></a>
+                            <a href="https://www.facebook.com/orquestrasanfonicabn/"><img className={classes.svg} src={require('../../../assets/svgs/facebookFooter.svg')} alt=""></img></a>
+                            <a href="https://www.instagram.com/orquestra.balaionordeste/"><img className={classes.svg} src={require('../../../assets/svgs/instagramFooter.svg')} alt=""></img></a>
                         </div>
                     </div>
                     <div className={classes.contact}>
-                        <h1>{this.props.language === 'portuguese' ? 'Entre em Contato!' : 
-                            this.props.language === 'english' ? 'Contact us!' : 
-                            this.props.language === 'french' ? 'Contactez nous!' : ''}</h1>
+                        <h1>{this.props.language === 'portuguese' ? 'Entre em Contato!' :
+                            this.props.language === 'english' ? 'Contact us!' :
+                                this.props.language === 'french' ? 'Contactez nous!' : ''}</h1>
                         <div className={classes.contactMedia}>
-                            <h2>contato1</h2>
-                            <h2>contato2</h2>
+                            <a><img className={classes.svg} src={require('../../../assets/svgs/whatsappFooter.svg')} alt=""></img></a>
+                            <a><img className={classes.svg} src={require('../../../assets/svgs/emailFooter.svg')} alt=""></img></a>
                         </div>
                     </div>
                 </div>
