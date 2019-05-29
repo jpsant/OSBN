0f564a1b5eab56d7284c8fcd18e0916a6385a339
Joao Paulo <j.paaulinho@gmail.com>
2019-05-29 17:09:29 -0300

    footer finalizado

--- src/Components/LandingPage/Footer/Footer.js
+++ src/Components/LandingPage/Footer/Footer.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import classes from './css/Footer.module.css';
 
 class Footer extends Component {
-    render () {
+    render() {
         return (
             <div className={classes.footerContainer}>
-                <h1>Footer!</h1>
+                <div className={classes.footerImage}>
+                    <img className={classes.logo} src={require('../../../assets/logo6.png')}></img>
+                </div>
+                <div className={classes.footerText}>
+                    <div className={classes.textContainer}>
+                        <h1 className={classes.text}>Orquestra Sanfônica Balaio Nordeste!</h1>
+                        <h1 className={classes.text}>&</h1>
+                        <h2>Copyright © OSBN 2018</h2>
+                    </div>
+                </div>
+                <div className={classes.contactText}>
+                    <div className={classes.container}>
+                        <h2>{this.props.language === 'portuguese' ? 'Desenvolvido e mantido por' :
+                            this.props.language === 'english' ? 'Developed and maintained by' : 
+                            this.props.language === 'french' ? 'Développé et maintenu par' : ''}</h2>
+                        <h1>João Paulo Santana</h1>
+                    </div>
+                    <div className={classes.socialMedia}>
+                        <a><img className={classes.svg} alt="" src={require('../../../assets/svgs/instagram.svg')}></img></a>
+                        <a><img className={classes.svg} alt="" src={require('../../../assets/svgs/github.svg')}></img></a>
+                        <a><img className={classes.svg} alt="" src={require('../../../assets/svgs/instagram.svg')}></img></a>
+                    </div>
+                </div>
             </div>
         )
     }
 }
 
-export default Footer;
+const mapStateToProps = state => {
+    return {
+        language: state.languageReducer.language
+    }
+}
+
+export default connect(mapStateToProps) (Footer);
