cfb763e0ad7d7327efd78d3438fe9db67c13450b
Joao Paulo <j.paaulinho@gmail.com>
2019-06-04 15:17:37 -0300

    galeria feita, faltando apenas o css

--- src/Components/Gallery/Gallery.js
+++ src/Components/Gallery/Gallery.js
@@ -1,28 +1,52 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
+import axios from 'axios';
 import classes from './css/Gallery.module.css';
 
 import LanguageSelector from '../UI/languageSelector/languageSelector';
+import TransitionDiv from '../UI/transitionDiv/history/historyDiv';
+import GalleryContent from '../UI/galleryContent/galleryContent';
 
 class Gallery extends Component {
 
+    componentDidMount() {
+        axios.get('https://osbn-a36f9.firebaseio.com/galeria.json')
+        .then(response => {
+            console.log(response.data)
+            this.setState({images: response.data})
+        })
+    }
+
+    state = {
+        images: null
+    }
+
     render() {
+
+        let images = null;
+        if(this.state.images) {
+            let image = this.state.images;
+            images = image.map(image => {
+                return <GalleryContent image={image.imagem} />
+            })
+        }
+        
         return (
-            <>
+            <div style={{ backgroundColor: '#AB7C44' }}>
                 <LanguageSelector />
                 <div className={classes.galleryContainer}>
-                <div className={classes.titleContainer}>
-                    <h1>{this.props.language === 'portuguese' ? '& Galeria' : 
-                    this.props.language === 'english' ? '& Gallery' : 
-                    this.props.language === 'french' ? '& Galerie' : ''}</h1>
-                </div>
-                <div className={classes.galleryContent}>
-                    <h1>content!</h1>
-                </div>
+                    <div className={classes.titleContainer}>
+                        <TransitionDiv title={this.props.language === 'portuguese' ? '& Galeria !' :
+                            this.props.language === 'english' ? '& Gallery !' :
+                                this.props.language === 'french' ? '& Galerie !' : ''} />
+                    </div>
+                    <div className={classes.galleryContent}>
+                        {images}
+                    </div>
                     <NavLink to="/" >voltar</NavLink>
                 </div>
-            </>
+            </div>
         )
     }
 }
---
+++ src/Components/UI/galleryContent/galleryContent.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import classes from './css/galleryContent.module.css';
+
+class GalleryContent extends Component {
+    render() {
+        return (
+            <div className={classes.item}>
+                <div className={classes.content}>
+                    <img src={this.props.image} alt="" className={classes.img}></img>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default GalleryContent;
