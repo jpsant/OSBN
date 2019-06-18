f875cc72e9d125b99fb689a8ed675cf5455c66b2
Joao Paulo <j.paaulinho@gmail.com>
2019-06-18 17:12:06 -0300

    finalizacao do fullpost

--- src/Components/Posts/NewsPost.js
+++ src/Components/Posts/NewsPost.js
@@ -2,35 +2,36 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
 
+import FullPost from '../UI/fullPost/fullPost';
+
 class NewsPost extends Component {
 
     componentDidMount() {
         axios.get('https://osbn-a36f9.firebaseio.com/noticias/' + this.props.language + '.json')
             .then(response => {
                 this.setState({ post: response.data, id: this.props.match.params.id });
-            })
+            });
     }
 
     state = {
         post: false,
-        id: null
+        id: null,
     }
 
     render() {
 
         let post = null;
         if (this.state.post) {
-            let result = null;
             let item = this.state.post;
             post = Object.values(item).map(item => {
-                return item.id === this.state.id ? console.log(item) : null;
+                return item.id === this.state.id ? <FullPost language={this.props.language} text={item.noticia} image={item.imagem} title={item.titulo} /> : null;
             })
         }
 
         return (
             <>
+                {/* <h1>post in: {this.props.language}, {this.props.match.params.id}</h1> */}
                 {post}
-                <h1>post in: {this.props.language}, {this.props.match.params.id}</h1>
             </>
         );
     }
---
+++ src/Components/UI/fullPost/fullPost.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import classes from './css/fullpost.module.css'
+
+import Title from '../transitionDiv/history/historyDiv';
+import Image from '../galleryImage/galleryImage';
+import Modal from '../modal/modal';
+import Backdrop from '../backDrop/backDrop';
+
+class FullPost extends Component {
+
+    state = {
+        showModal: false,
+        image: null
+    }
+
+    openHandler = (image) => {
+        this.setState({ showModal: true, image: image })
+    }
+
+    closeHandler = () => {
+        this.setState({ showModal: false, image: null });
+    }
+
+    render() {
+        return (
+            <div style={{backgroundColor: '#AC7C44'}}>
+                <Title title={this.props.title} />
+                <div className={classes.postContainer}>
+                    <div className={classes.imageContainer}>
+                        <Image image={this.props.image} clicked={() => this.openHandler(this.props.image)}/>
+                    </div>
+                    <div className={classes.textContainer}>
+                        <h2>{this.props.text}</h2>
+                        <NavLink to="/" className={classes.Button}>{this.props.language === 'portuguese' ? '& Voltar' : 
+                        this.props.language === 'english' ? '& Home' : 
+                        this.props.language === 'french' ? '& Volver' : null}</NavLink>
+                    </div>
+                </div>
+                <Backdrop show={this.state.showModal} clicked={this.closeHandler} />
+                <Modal show={this.state.showModal}>
+                    <div className={classes.imgContainer}>
+                        <img className={classes.img} alt="" src={this.state.image}></img>
+                    </div>
+                </Modal>
+            </div>
+        );
+    }
+}
+
+export default FullPost;
