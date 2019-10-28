b0a2104aeb9806a2a6a0e5516145f19417f96acd
João Paulo <j.paaulinho@gmail.com>
2019-10-27 23:27:33 -0300

    ajuste na responsividade dos cards de agenda

--- src/Components/UI/scheduleDiv/scheduleDiv.js
+++ src/Components/UI/scheduleDiv/scheduleDiv.js
@@ -4,17 +4,18 @@ import classes from './css/scheduleDiv.module.css';
 
 const scheduleDiv = (props) => {
     return (
-        <a href={props.link} className={classes.container}>
+        <div className={classes.container}>
             <div className={classes.date}>
                 <h1>{props.date}</h1>
             </div>
             <div className={classes.event}>
                 <h2>{props.event}</h2>
+                <a className={classes.button} href={props.link}> Comprar Ingresso</a>
             </div>
             <div className={classes.local}>
                 <h2>{props.local}</h2>
             </div>
-        </a>
+        </div>
     );
 }
 
