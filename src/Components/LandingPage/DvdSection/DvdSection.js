8cfd0c75b2fad942c2d5637ef56bf1d3f6e29fe0
João Paulo <j.paaulinho@gmail.com>
2019-11-14 18:35:16 -0300

    loading inicial das animações corrigido utilizando uma div contendo as animacoes das duas sections finais e uma animacao que seta a opacidade inicial desses svgs para 0 durante dois segundos

--- src/Components/LandingPage/DvdSection/DvdSection.js
+++ src/Components/LandingPage/DvdSection/DvdSection.js
@@ -28,25 +28,27 @@ class DvdSection extends Component {
     render() {
         return (
             <>
-                <div>
-                    <LeftUp show={this.state.currentPage === 1 ? true : false} />
-                    <RightUp show={this.state.currentPage === 1 ? true : false} />
-                    <LeftDown show={this.state.currentPage === 1 ? true : false} />
-                    <RightDown show={this.state.currentPage === 1 ? true : false} />
+                <LeftUp show={this.state.currentPage === 1 ? true : false} />
+                <RightUp show={this.state.currentPage === 1 ? true : false} />
+                <LeftDown show={this.state.currentPage === 1 ? true : false} />
+                <RightDown show={this.state.currentPage === 1 ? true : false} />
 
+                <div className={classes.animationsContainer}>
                     <DownArabesco show={this.state.currentPage === 2 ? true : false} />
                     <UpArabesco show={this.state.currentPage === 2 ? true : false} />
 
                     <FullUp show={this.state.currentPage === 3 ? true : false} />
                     <FullDown show={this.state.currentPage === 3 ? true : false} />
-                    <ReactPageScroller pageOnChange={this.handlePageChange}>
-                        <div className={classes.section1}>
-                            <div className={classes.container}>
-                                <video className={classes.video} autoPlay="autoplay" loop="loop" muted playsInline poster={'https://firebasestorage.googleapis.com/v0/b/osbn-a36f9.appspot.com/o/imagens%2FFoto%20-%20divulga%C3%A7%C3%A3o%206.png?alt=media&token=38d4a012-ac89-4d9d-bdad-160ced9e022b'}>
-                                    <source src={'https://firebasestorage.googleapis.com/v0/b/osbn-a36f9.appspot.com/o/videos%2Forquestra2.mp4?alt=media&token=bf248dca-0c2b-4738-99d6-0aee3e27d860'} type='video/mp4' />
-                                </video>
-                            </div>
-                            {/* <Fade top duration={3500}>
+                </div>
+
+                <ReactPageScroller pageOnChange={this.handlePageChange}>
+                    <div className={classes.section1}>
+                        <div className={classes.container}>
+                            <video className={classes.video} autoPlay="autoplay" loop="loop" muted playsInline poster={'https://firebasestorage.googleapis.com/v0/b/osbn-a36f9.appspot.com/o/imagens%2FFoto%20-%20divulga%C3%A7%C3%A3o%206.png?alt=media&token=38d4a012-ac89-4d9d-bdad-160ced9e022b'}>
+                                <source src={'https://firebasestorage.googleapis.com/v0/b/osbn-a36f9.appspot.com/o/videos%2Forquestra2.mp4?alt=media&token=bf248dca-0c2b-4738-99d6-0aee3e27d860'} type='video/mp4' />
+                            </video>
+                        </div>
+                        {/* <Fade top duration={3500}>
                             <div className={classes.titleContainer}>
                                 <h1 className={classes.hat}>&</h1>
                                 <h1>Orquestra Sanfônica Balaio Nordeste</h1>
@@ -57,18 +59,18 @@ class DvdSection extends Component {
                                 <h1>DVD Promocional.</h1>
                             </div>
                         </Fade> */}
+                    </div>
+                    <div className={classes.section2}>
+                        <div className={classes.container}>
+                            <video className={classes.video} autoPlay="autoplay" loop="loop" muted playsInline poster={'https://firebasestorage.googleapis.com/v0/b/osbn-a36f9.appspot.com/o/imagens%2FFoto%20-%20divulga%C3%A7%C3%A3o%206.png?alt=media&token=38d4a012-ac89-4d9d-bdad-160ced9e022b'}>
+                                <source src={'https://firebasestorage.googleapis.com/v0/b/osbn-a36f9.appspot.com/o/videos%2Forquestra2.mp4?alt=media&token=bf248dca-0c2b-4738-99d6-0aee3e27d860'} type='video/mp4' />
+                            </video>
                         </div>
-                        <div className={classes.section2}>
-                            <div className={classes.container}>
-                                <video className={classes.video} autoPlay="autoplay" loop="loop" muted playsInline poster={'https://firebasestorage.googleapis.com/v0/b/osbn-a36f9.appspot.com/o/imagens%2FFoto%20-%20divulga%C3%A7%C3%A3o%206.png?alt=media&token=38d4a012-ac89-4d9d-bdad-160ced9e022b'}>
-                                    <source src={'https://firebasestorage.googleapis.com/v0/b/osbn-a36f9.appspot.com/o/videos%2Forquestra2.mp4?alt=media&token=bf248dca-0c2b-4738-99d6-0aee3e27d860'} type='video/mp4' />
-                                </video>
-                            </div>
-                            {/* <LeftUp show={this.state.currentPage === 2 ? true : false} top="100%" />
+                        {/* <LeftUp show={this.state.currentPage === 2 ? true : false} top="100%" />
                         <RightUp show={this.state.currentPage === 2 ? true : false} top="100%"/>
                         <LeftDown show={this.state.currentPage === 2 ? true : false} bottom="-100%" />
                         <RightDown show={this.state.currentPage === 2 ? true : false} bottom="-100%"/> */}
-                            {/* <Fade top duration={3500}>
+                        {/* <Fade top duration={3500}>
                             <div className={classes.titleContainer}>
                                 <h1 className={classes.hat}>&</h1>
                                 <h1>Orquestra Sanfônica Balaio Nordeste</h1>
@@ -79,18 +81,18 @@ class DvdSection extends Component {
                                 <h1>DVD Promocional da Orquestra.</h1>
                             </div>
                         </Fade> */}
+                    </div>
+                    <div className={classes.section3}>
+                        <div className={classes.container}>
+                            <video className={classes.video} autoPlay="autoplay" loop="loop" muted playsInline poster={'https://firebasestorage.googleapis.com/v0/b/osbn-a36f9.appspot.com/o/imagens%2FFoto%20-%20divulga%C3%A7%C3%A3o%206.png?alt=media&token=38d4a012-ac89-4d9d-bdad-160ced9e022b'}>
+                                <source src={'https://firebasestorage.googleapis.com/v0/b/osbn-a36f9.appspot.com/o/videos%2Forquestra2.mp4?alt=media&token=bf248dca-0c2b-4738-99d6-0aee3e27d860'} type='video/mp4' />
+                            </video>
                         </div>
-                        <div className={classes.section3}>
-                            <div className={classes.container}>
-                                <video className={classes.video} autoPlay="autoplay" loop="loop" muted playsInline poster={'https://firebasestorage.googleapis.com/v0/b/osbn-a36f9.appspot.com/o/imagens%2FFoto%20-%20divulga%C3%A7%C3%A3o%206.png?alt=media&token=38d4a012-ac89-4d9d-bdad-160ced9e022b'}>
-                                    <source src={'https://firebasestorage.googleapis.com/v0/b/osbn-a36f9.appspot.com/o/videos%2Forquestra2.mp4?alt=media&token=bf248dca-0c2b-4738-99d6-0aee3e27d860'} type='video/mp4' />
-                                </video>
-                            </div>
-                            {/* <LeftUp show={this.state.currentPage === 3 ? true : false} top="200%" />
+                        {/* <LeftUp show={this.state.currentPage === 3 ? true : false} top="200%" />
                         <RightUp show={this.state.currentPage === 3 ? true : false} top="200%"/>
                         <LeftDown show={this.state.currentPage === 3 ? true : false} bottom="-200%" />
                         <RightDown show={this.state.currentPage === 3 ? true : false} bottom="-200%"/> */}
-                            {/* <Fade top duration={3500}>
+                        {/* <Fade top duration={3500}>
                             <div className={classes.titleContainer}>
                                 <h1 className={classes.hat}>&</h1>
                                 <h1>Orquestra Sanfônica Balaio Nordeste</h1>
@@ -101,9 +103,8 @@ class DvdSection extends Component {
                                 <h1>DVD Promocional da Orquestra.</h1>
                             </div>
                         </Fade> */}
-                        </div>
-                    </ReactPageScroller>
-                </div>
+                    </div>
+                </ReactPageScroller>
             </>
         )
     }
