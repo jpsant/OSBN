f035bca8a0d6c2871d2fc624457eb3640ebabecb
João Paulo <j.paaulinho@gmail.com>
2019-11-21 15:59:14 -0300

    finalizacao dos componentes animados restantes, criacao da section 4 da pagina do DVD

--- src/Components/LandingPage/DvdSection/DvdSection.js
+++ src/Components/LandingPage/DvdSection/DvdSection.js
@@ -23,6 +23,9 @@ import Drum from '../../UI/animated-components/animated-drum/drum';
 import Triangle from '../../UI/animated-components/animated-triangle/triangle';
 import Flute from '../../UI/animated-components/animated-flute/flute';
 
+import LateralLeft from '../../UI/animated-components/animated-lateral-left/lateralLeft';
+import LateralRight from '../../UI/animated-components/animated-lateral-right/lateralRight';
+
 class DvdSection extends Component {
 
     state = {
@@ -41,6 +44,10 @@ class DvdSection extends Component {
                 <LeftDown show={this.state.currentPage === 1 ? true : false} />
                 <RightDown show={this.state.currentPage === 1 ? true : false} />
 
+                {/* Como os arquivos CSS sem o .module afetam a aplicação inteira tomei o cuidado de utilizar nomes únicos para as classes
+                    E as classes e animações dos Arabescos estão no arquivo CSS do componente 'Arabesco-left-up'.
+                */}
+
                 <div className={classes.animationsContainer}>
                     <DownArabesco show={this.state.currentPage === 2 ? true : false} />
                     <UpArabesco show={this.state.currentPage === 2 ? true : false} />
@@ -56,7 +63,10 @@ class DvdSection extends Component {
                     <Accordion show={this.state.currentPage === 3 ? true : false}  />
                     <Drum show={this.state.currentPage === 3 ? true : false} />
                     <Flute show={this.state.currentPage === 3 ? true : false}  />
-                    {/* <Triangle show={this.state.currentPage === 3 ? true : false}  /> */}
+                    <Triangle show={this.state.currentPage === 3 ? true : false}  />
+
+                    <LateralLeft show={this.state.currentPage === 4 ? true : false} />
+                    <LateralRight show={this.state.currentPage === 4 ? true : false} />
                     
                 </div>
 
@@ -99,7 +109,16 @@ class DvdSection extends Component {
                             <h2 className={this.state.currentPage === 3 ? classes.text : classes.textOut}>Ao som de Cirandas, Côco de Roda, Baião, Xote e muito Forró
                             A Orquestra Sanfônica Balaio Nordeste divide o palco com grandes mestres da Música Brasileira, como Luiz Gonzaga, Jackson do Pandeiro, Chico César, Sivuca, dentre outros.</h2>
                         </div>
-
+                    </div>
+                    <div className={classes.section4}>
+                        <div className={classes.container}>
+                            <video className={classes.video} autoPlay="autoplay" loop="loop" muted playsInline poster={'https://firebasestorage.googleapis.com/v0/b/osbn-a36f9.appspot.com/o/imagens%2FFoto%20-%20divulga%C3%A7%C3%A3o%206.png?alt=media&token=38d4a012-ac89-4d9d-bdad-160ced9e022b'}>
+                                <source src={'https://firebasestorage.googleapis.com/v0/b/osbn-a36f9.appspot.com/o/videos%2Forquestra2.mp4?alt=media&token=bf248dca-0c2b-4738-99d6-0aee3e27d860'} type='video/mp4' />
+                            </video>
+                        </div>
+                        <div className={classes.textContainer}>
+                            
+                        </div>
                     </div>
                 </ReactPageScroller>
             </>
