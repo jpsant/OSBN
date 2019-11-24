ae5479e6c57dae2122d57bcc85e05cac311bc3cb
João Paulo <j.paaulinho@gmail.com>
2019-11-24 11:40:13 -0300

    atualizacao das informacoes da orquestra para frances e ingles

--- src/Components/LandingPage/InfoSection/InfoSection.js
+++ src/Components/LandingPage/InfoSection/InfoSection.js
@@ -35,37 +35,27 @@ class InfoSection extends Component {
                                 <h2>
                                     {this.props.language === 'portuguese' ? `A Orquestra Sanfônica Balaio Nordeste é o produto de maior visibilidade da entidade e surgiu da necessidade de inovação no cenário 
                             musical e hoje difunde a cultura nordestina e particularmente a paraibana por onde passa.` :
-                                        this.props.language === 'english' ? `The Orchestra Sanfônica Balaio Nordeste originated in an accordion workshop held by the "Balaio Nordeste Cultural Association" and FUNJOPE (Cultural Foundation of João Pessoa) in March 2010. With its own arrangements and musical production of maestro Lucílio and some of the participating musicians,
-                            the Orchestra has established itself as one of the most genuine and expressive cultural products of northeastern music, and counts, in its current formation, with 14 members:
-                            the maestro, eight accordionists, two flute players and three percussionists.` :
+                                        this.props.language === 'english' ? `The Balaio Nordeste Orchestra is the most visible product of the entity and emerged from the need for innovation in the music scene and today spreads the northeastern Brazilian culture and particularly the Paraibana where it passes.` :
                                             this.props.language === 'french' ? `
-                            Le Orquestra Sanfônica Balaio Nordeste a été créé en mars 2010 lors d'un atelier d'accordéon organisé par l'association culturelle Balaio Nordeste et la FUNJOPE (Fondation culturelle de João Pessoa).
-                            L’Orchestre s’est établi comme l’un des produits culturels les plus authentiques et les plus expressifs de la musique du Nord-Est et compte, dans sa formation actuelle, 14 membres:
-                            le maestro, huit accordéonistes, deux flûtistes et trois percussionnistes.` : ''}
+                                            Le Balaio Nordeste Orchestra est le produit le plus visible de l’entité. Il découle du besoin d’innovation sur la scène musicale et propage aujourd’hui dans le nord-est de la culture brésilienne, et en particulier dans la région de Paraibana.` : ''}
                                 </h2>
                                 <h2>
                                     {this.props.language === 'portuguese' ? `Com dois DVD’s no currículo, rompeu as fronteiras do Brasil e se apresentou no maior festival de acordeon da França, retornando no ano seguinte como convidada de honra. Em 2017 
                             Orquestra foi contemplada pelo programa Ibermúsicas e viajou para o Peru, onde se apresentou no festival FESTIFOLCOR, em Arequipa..` :
-                                        this.props.language === 'english' ? `The Orchestra has performed in several Brazilian states and for two consecutive years (2015/2016) in France, where it participated in the country's largest accordion festival,
-                            "Le Printemps des Bretelles", and in 2016, at the invitation of the Cultural Sector of the Embassy of Brazil in France, also held a concert on the opening night of the international book fair,
-                            "Livre Paris 2016", performing at the Teatro Lúcio Costa, at the "Maison du Brésil" Foundation in Paris.` :
-                                            this.props.language === 'french' ? `L'Orchestre s'est produit dans plusieurs États brésiliens et deux années consécutives (2015/2016) en France, où il a participé au plus grand festival d'accordéon du pays.
-                            "Le Printemps des Bretelles" et en 2016, à l'invitation du secteur culturel de l'ambassade du Brésil en France, ont également donné un concert lors de la soirée d'ouverture du salon international du livre,
-                            "Livre Paris 2016", au Teatro Lúcio Costa, à la Fondation "Maison du Brésil" à Paris.` : ''}
+                                        this.props.language === 'english' ? `With two DVDs in the curriculum, the Orchestra broke the borders of Brazil and performed at the largest accordion festival in France, returning the following year as a guest of honor. In 2017 the Orchestra was awarded by the Ibermúsicas program and traveled to Peru, where it performed at the FESTIFOLCOR festival in Arequipa.` :
+                                            this.props.language === 'french' ? `Avec deux DVD au programme, l'orchestre a brisé les frontières du Brésil et s'est produit lors du plus grand festival d'accordéon en France, avant de revenir l'année suivante en tant qu'invité d'honneur. En 2017, l'Orchestre a été récompensé par le programme Ibermúsicas et s'est rendu au Pérou, où il s'est produit lors du festival FESTIFOLCOR à Arequipa.` : ''}
                                 </h2>
                                 <h2>
                                     {
                                         this.props.language === 'portuguese' ? 'Já se apresentou nos mais variados palcos do Brasil, participando de projetos que fomentam a música regional.' :
-                                            this.props.language === 'english' ? 'In May 2017, the Orchestra was contemplated by the Ibermúsicas program and traveled to Peru where it performed at the FESTIFOLCOR festival in Arequipa.' :
-                                                this.props.language === 'french' ? 'En mai 2017, le programme Ibermúsicas a permis à l\'orchestre de se rendre au Pérou, où il s\'est produit lors du festival FESTIFOLCOR à Arequipa.' : ''
+                                            this.props.language === 'english' ? 'The Orchestra has performed on various stages in Brazil, participating in projects that promote regional music.' :
+                                                this.props.language === 'french' ? 'L\'Orchestre s\'est produit sur diverses scènes au Brésil, participant à des projets de promotion de la musique régionale.' : ''
                                     }
                                 </h2>
                                 <h2>
                                     {this.props.language === 'portuguese' ? `O desafio é fazer com que a OSBN tenha um calendário permanente de apresentações, seja no estado, seja nos palcos do Brasil e de outros países` :
-                                        this.props.language === 'english' ? `In June of 2017, he recorded in the Theatro Santa Roza, in João Pessoa, the DVD "Orphestra Sanfônica in Ritmos de Nordeste" with the participation of renowned artists like Irah Caldeira,
-                            Pinto do Acordeon, Cezzinha and Silvério Pessoa.` :
-                                            this.props.language === 'french' ? `En juin 2017, il a enregistré au Theatro Santa Roza, à João Pessoa, le DVD "Orphestra Sanfônica in Ritmos de Nordeste", avec la participation d'artistes de renom tels que Irah Caldeira,
-                            Pinto do Acordeon, Cezzinha et Silvério Pessoa.` : ''}
+                                        this.props.language === 'english' ? `The challenge is to make OSBN have a permanent schedule of presentations, either in the state or on the stage in Brazil and other countries.` :
+                                            this.props.language === 'french' ? `Le défi consiste à faire en sorte que OSBN ait un calendrier de présentations permanent, soit dans l’état, soit au Brésil et dans d’autres pays.` : ''}
                                 </h2>
                                 <h2>
                                     {this.props.language === 'portuguese' ? `O repertório da Orquestra apresenta grandes mestres da música brasileira, como Luiz Gonzaga, Jackson do Pandeiro, Chico César,  Sivuca, dentre outros, levando o público a cantar e dançar ao som de cirandas,
