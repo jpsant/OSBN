8bcd6ef3a80ed1c3286b769f7c489adf0cc4388a
João Paulo <j.paaulinho@gmail.com>
2019-11-28 15:13:30 -0300

    pager da section do DVD adicionado e funcional

--- src/Components/LandingPage/DvdSection/DvdSection.js
+++ src/Components/LandingPage/DvdSection/DvdSection.js
@@ -32,11 +32,11 @@ import Pagination from '../../UI/dvdPagination/pagination';
 class DvdSection extends Component {
 
     state = {
-        currentPage: 1
+        currentPage: 0
     }
 
     handlePageChange = (number) => {
-        this.setState({ currentPage: number }); // set currentPage number, to reset it from the previous selected.
+        this.setState({ currentPage: number}); // set currentPage number, to reset it from the previous selected.
     };
 
     render() {
@@ -46,40 +46,40 @@ class DvdSection extends Component {
                     <title>Orquestra Sanfônica Balaio Nordeste DVD</title>
                     <meta name="description" content="Página Promocional do DVD da Orquestra Sanfônica Balaio Nordeste"></meta>
                 </Helmet>
-                <LeftUp show={this.state.currentPage === 1 ? true : false} />
-                <RightUp show={this.state.currentPage === 1 ? true : false} />
-                <LeftDown show={this.state.currentPage === 1 ? true : false} />
-                <RightDown show={this.state.currentPage === 1 ? true : false} />
+                <LeftUp show={this.state.currentPage === 0 ? true : false} />
+                <RightUp show={this.state.currentPage === 0 ? true : false} />
+                <LeftDown show={this.state.currentPage === 0 ? true : false} />
+                <RightDown show={this.state.currentPage === 0 ? true : false} />
 
                 {/* Como os arquivos CSS sem o .module afetam a aplicação inteira tomei o cuidado de utilizar nomes únicos para as classes
                     E as classes e animações dos Arabescos estão no arquivo CSS do componente 'Arabesco-left-up'.
                 */}
 
                 <div className={classes.animationsContainer}>
-                    <DownArabesco show={this.state.currentPage === 2 ? true : false} />
-                    <UpArabesco show={this.state.currentPage === 2 ? true : false} />
+                    <DownArabesco show={this.state.currentPage === 1 ? true : false} />
+                    <UpArabesco show={this.state.currentPage === 1 ? true : false} />
 
-                    <Dancers show={this.state.currentPage === 2 ? true : false} />
-                    <Bird show={this.state.currentPage === 2 ? true : false} />
-                    <Sky show={this.state.currentPage === 2 ? true : false} />
-                    <Cactus show={this.state.currentPage === 2 ? true : false} />
+                    <Dancers show={this.state.currentPage === 1 ? true : false} />
+                    <Bird show={this.state.currentPage === 1 ? true : false} />
+                    <Sky show={this.state.currentPage === 1 ? true : false} />
+                    <Cactus show={this.state.currentPage === 1 ? true : false} />
 
-                    <FullUp show={this.state.currentPage === 3 ? true : false} />
-                    <FullDown show={this.state.currentPage === 3 ? true : false} />
+                    <FullUp show={this.state.currentPage === 2 ? true : false} />
+                    <FullDown show={this.state.currentPage === 2 ? true : false} />
 
-                    <Accordion show={this.state.currentPage === 3 ? true : false} />
-                    <Drum show={this.state.currentPage === 3 ? true : false} />
-                    <Flute show={this.state.currentPage === 3 ? true : false} />
-                    <Triangle show={this.state.currentPage === 3 ? true : false} />
+                    <Accordion show={this.state.currentPage === 2 ? true : false} />
+                    <Drum show={this.state.currentPage === 2 ? true : false} />
+                    <Flute show={this.state.currentPage === 2 ? true : false} />
+                    <Triangle show={this.state.currentPage === 2 ? true : false} />
 
-                    <LateralLeft show={this.state.currentPage === 4 ? true : false} />
-                    <LateralRight show={this.state.currentPage === 4 ? true : false} />
+                    <LateralLeft show={this.state.currentPage === 3 ? true : false} />
+                    <LateralRight show={this.state.currentPage === 3 ? true : false} />
 
-                    <Pagination page={this.state.currentPage} />
+                    <Pagination changePage={this.handlePageChange} page={this.state.currentPage} />
 
                 </div>
 
-                <ReactPageScroller pageOnChange={this.handlePageChange}>
+                <ReactPageScroller pageOnChange={this.handlePageChange} customPageNumber={this.state.currentPage} >
                     <div className={classes.section1}>
                         <div className={classes.container}>
                             <video className={classes.video} autoPlay="autoplay" loop="loop" muted playsInline poster={'https://firebasestorage.googleapis.com/v0/b/osbn-a36f9.appspot.com/o/imagens%2FFoto%20-%20divulga%C3%A7%C3%A3o%206.png?alt=media&token=38d4a012-ac89-4d9d-bdad-160ced9e022b'}>
@@ -87,11 +87,11 @@ class DvdSection extends Component {
                             </video>
                         </div>
                         <div className={classes.titleContainer}>
-                            <h1 className={this.state.currentPage === 1 ? classes.hat : classes.hatOut}>&</h1>
-                            <h1 className={this.state.currentPage === 1 ? classes.title : classes.titleOut}>Orquestra Sanfônica Balaio Nordeste</h1>
+                            <h1 className={this.state.currentPage === 0 ? classes.hat : classes.hatOut}>&</h1>
+                            <h1 className={this.state.currentPage === 0 ? classes.title : classes.titleOut}>Orquestra Sanfônica Balaio Nordeste</h1>
                         </div>
                         <div className={classes.bodyContainer}>
-                            <h1 className={this.state.currentPage === 1 ? classes.subtitle : classes.subtitleOut}>DVD Promocional.</h1>
+                            <h1 className={this.state.currentPage === 0 ? classes.subtitle : classes.subtitleOut}>DVD Promocional.</h1>
                         </div>
                     </div>
                     <div className={classes.section2}>
@@ -101,8 +101,8 @@ class DvdSection extends Component {
                             </video>
                         </div>
                         <div className={classes.textContainer}>
-                            <h1 className={this.state.currentPage === 2 ? classes.hat : classes.hatOut}>&</h1>
-                            <h2 className={this.state.currentPage === 2 ? classes.text : classes.textOut}>A Orquestra Sanfônica Balaio Nordeste difunde a Cultura Nordestina, apresentando
+                            <h1 className={this.state.currentPage === 1 ? classes.hat : classes.hatOut}>&</h1>
+                            <h2 className={this.state.currentPage === 1 ? classes.text : classes.textOut}>A Orquestra Sanfônica Balaio Nordeste difunde a Cultura Nordestina, apresentando
                             seus valores e mantendo viva a sua tradição e de seus personagens.</h2>
                         </div>
 
@@ -114,8 +114,8 @@ class DvdSection extends Component {
                             </video>
                         </div>
                         <div className={classes.textContainer}>
-                            <h1 className={this.state.currentPage === 3 ? classes.hat : classes.hatOut}>&</h1>
-                            <h2 className={this.state.currentPage === 3 ? classes.text : classes.textOut}>Ao som de Cirandas, Côco de Roda, Baião, Xote e muito Forró
+                            <h1 className={this.state.currentPage === 2 ? classes.hat : classes.hatOut}>&</h1>
+                            <h2 className={this.state.currentPage === 2 ? classes.text : classes.textOut}>Ao som de Cirandas, Côco de Roda, Baião, Xote e muito Forró
                             A Orquestra Sanfônica Balaio Nordeste divide o palco com grandes mestres da Música Brasileira, como Luiz Gonzaga, Jackson do Pandeiro, Chico César, Sivuca, dentre outros.</h2>
                         </div>
                     </div>
--- src/Components/UI/dvdPagination/pagination.js
+++ src/Components/UI/dvdPagination/pagination.js
@@ -5,11 +5,10 @@ const Pagination = (props) => {
     return (
         <>
 <ul className={classes.pagination}>
-  <li className={props.page === 1 ? classes.selectedPage : ''} ><a href="#page1"></a></li>
-  <li className={props.page === 2 ? classes.selectedPage : ''}><a href="#page2"></a></li>
-  <li className={props.page === 3 ? classes.selectedPage : ''}><a href="#page3"></a></li>
-  <li className={props.page === 4 ? classes.selectedPage : ''}><a href="#page4"></a></li>
-  {/* <li><a href="#page5" className={props.page === 1 ? classes.selectedPage : ''}></a></li> */}
+  <li className={props.page === 0 ? classes.selectedPage : ''} ><a onClick={() => props.changePage(0)}></a></li>
+  <li className={props.page === 1 ? classes.selectedPage : ''}><a onClick={() => props.changePage(1)}></a></li>
+  <li className={props.page === 2 ? classes.selectedPage : ''}><a onClick={() => props.changePage(2)}></a></li>
+  <li className={props.page === 3 ? classes.selectedPage : ''}><a onClick={() => props.changePage(3)}></a></li>
 </ul>
         </>
     )
