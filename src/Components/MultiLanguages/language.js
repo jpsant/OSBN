edd4fc9b45efe82d9eb5105ce3b4f212c758f8d7
João Paulo <j.paaulinho@gmail.com>
2020-06-25 16:34:15 -0300

    created a file to contain all the language texts of the application

---
+++ src/Components/MultiLanguages/language.js
@@ -0,0 +1,11 @@
+import Portuguese from './languages/portuguese';
+import English from './languages/english';
+import French from './languages/french';
+
+let languages = {
+  "portuguese": Portuguese,
+  "english" : English,
+  "french": French
+}
+
+export default languages;
---
+++ src/Components/MultiLanguages/languages/french.js
@@ -0,0 +1,74 @@
+let french = {
+
+  helmet: `Website da Orquestra Sanfônica Balaio Nordeste`,
+
+  landingPage: {
+    infoSection: {
+      transitionDiv: `L\'orchestre`,
+      infoContainer__text1: `Le Balaio Nordeste Orchestra est le produit le plus visible de l’entité. Il découle du besoin d’innovation sur la scène musicale et propage aujourd’hui dans le nord-est de la culture brésilienne, et en particulier dans la région de Paraibana.`,
+      infoContainer__text2: `Avec deux DVD au programme, l'orchestre a brisé les frontières du Brésil et s'est produit lors du plus grand festival d'accordéon en France, avant de revenir l'année suivante en tant qu'invité d'honneur. En 2017, l'Orchestre a été récompensé par le programme Ibermúsicas et s'est rendu au Pérou, où il s'est produit lors du festival FESTIFOLCOR à Arequipa.`,
+      infoContainer__text3: `L'Orchestre s'est produit sur diverses scènes au Brésil, participant à des projets de promotion de la musique régionale.`,
+      infoContainer__text4: `Le défi consiste à faire en sorte que OSBN ait un calendrier de présentations permanent, soit dans l’état, soit au Brésil et dans d’autres pays.`,
+      infoContainer__text5: `Le répertoire de l'orchestre présente de grands maîtres de la musique brésilienne, tels que Luiz Gonzaga, Jackson do Pandeiro, Chico César, Sivuca, entre autres, amenant le public à chanter et à danser au son des cirandas, coco de roda, baião, xote, et beaucoup pour foro.`,
+    },
+
+    historySection: {
+      transitionDiv: `Historique`,
+    },
+
+    gallerySection: {
+      transitionDiv: `Galerie`,
+      galleryButton: `Aller à la Galerie!`,
+    },
+
+    scheduleSection: {
+      transitionDiv: `Agenda`,
+    },
+
+    newsSection: {
+      transitionDiv: `Nouvelles`,
+      newsAlert: `Il n\'y a actuellement aucune nouvelle disponible`,
+    },
+
+    technicalSection: {
+      transitionDiv: `Répertoire`,
+      musicContainer__table__music: `Musique`,
+      musicContainer__table__composer: `Compositeur`,
+    },
+
+    contactSection: {
+      transitionDiv: `Nous contacter`,
+      social: `Suivez nous sur les réseaux sociaux!`,
+      contact: `Contactez nous`,
+      wpp: `Contact de production: (83) 99922-3749`
+    },
+  },
+
+  gallery: {
+    helmet: `Galerie d'images de l'Orchestre Balaio Nordeste Sanfônica`,
+    transitionDiv: `Galerie`,
+    button: `& Début`,
+  },
+
+  dvd: {
+    meta: `Page promotionnelle du DVD Balaio Nordeste Sanfônica Orquestra`,
+    meet: `Rencontrez l\'orchestre!`,
+    dvd__title: `DVD promotionnel.`,
+    dvd__text1: `Le Orchestra Sanfônica Balaio Nordeste propage la culture brésilien du Nord-Est, en présentant ses valeurs et en maintenant sa tradition et ses personnages.`,
+    dvd__text2: `Au son de Cirandas, Côco de roda, Baião, Xote et beaucoup Forró, l\'Orchestre symphonique Balaio Nordeste partage la scène avec Irah Caldeira, Yure Carvalho, Pinto do Acordeon, Cezzinha et Silverio Pessoa avec les classiques des grands maîtres de la musique brésilienne, comme Luiz Gonzaga, Jackson Pandeiro, Chico César, Sivuca, entre autres.`,
+    dvd__text3: `Déjà disponible au format numérique sur les plateformes de streaming. Venez jeter un coup d\'œil et honorer la véritable culture du Nord-Est brésilien!`,
+  },
+
+  sideMenu: {
+    openMenu: `Ouvrir le menu`,
+    closeMenu: `Fermer le menu`,
+    historyButton: `Histoire`,
+    galleryButton: `Galerie`,
+    scheduleButton: `Agenda`,
+    newsButton: `Nouvelles`,
+    technicalButton: `Répertoire`,
+    socialButton: `Social`,
+  }
+};
+
+export default french;
